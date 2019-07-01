3308c33f3d8a90073552801daf9a4906f563f184
Junpei Tsuji <tsuji_jumpei@cyberagent.co.jp>
2019-07-01 13:51:49 +0900

    Added no auth plugin

---
+++ go-plugin-rpc/plugin/no-auth/plugin.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"os"
+
+	"github.com/hashicorp/go-hclog"
+	"github.com/hashicorp/go-plugin"
+	"github.com/jun06t/go-sample/go-plugin-rpc/common"
+)
+
+const (
+	password = "hoge"
+)
+
+type PasswordAuth struct {
+	logger hclog.Logger
+}
+
+func (p *PasswordAuth) Authenticate() bool {
+	return true
+}
+
+func main() {
+	logger := hclog.New(&hclog.LoggerOptions{
+		Level:      hclog.Info,
+		Output:     os.Stderr,
+		JSONFormat: true,
+	})
+
+	auth := &PasswordAuth{
+		logger: logger,
+	}
+	// pluginMap is the map of plugins we can dispense.
+	var pluginMap = map[string]plugin.Plugin{
+		"authPlugin": &common.AuthPlugin{Impl: auth},
+	}
+
+	plugin.Serve(&plugin.ServeConfig{
+		HandshakeConfig: common.HandshakeConfig,
+		Plugins:         pluginMap,
+	})
+}
