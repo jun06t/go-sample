0162c92168f3a29ba55e5711725b7d6a61f0a60a
Junpei Tsuji <junpei.tsuji.ams@gmail.com>
2019-11-06 01:59:37 +0900

    Handle SIGPIPE

--- pipe/head/main.go
+++ pipe/head/main.go
@@ -3,18 +3,35 @@ package main
 import (
 	"errors"
 	"fmt"
+	"os"
+	"os/signal"
 	"syscall"
 )
 
 func main() {
-	for {
-		_, err := fmt.Println("Wow!")
-		if err != nil {
-			if errors.Is(err, syscall.EPIPE) {
-				break
-			} else {
-				panic(err)
+	signal_chan := make(chan os.Signal, 1)
+	signal.Notify(signal_chan, syscall.SIGPIPE)
+
+	go func() {
+		select {
+		case s := <-signal_chan:
+			_, err := fmt.Println("catch signal")
+			if err != nil {
+				if errors.Is(err, syscall.EPIPE) {
+					fmt.Fprintln(os.Stderr, "catch broken pipe error")
+				} else {
+					fmt.Fprintln(os.Stderr, err)
+				}
 			}
+			fmt.Fprintln(os.Stderr, s)
 		}
+	}()
+
+	run()
+}
+
+func run() {
+	for {
+		fmt.Println("Wow!")
 	}
 }
