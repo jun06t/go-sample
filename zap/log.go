27316b693693f6c36cf9989acf7bdb64ebb4558f
Junpei Tsuji <junpei.tsuji.ams@gmail.com>
2021-09-22 10:45:50 +0900

    feat: add zap sample

---
+++ zap/log.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"os"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+func main() {
+	{
+		logger := NewMultiOutputLogger()
+		logger.Error("error message")
+		logger.Info("info message")
+		logger.Debug("debug message")
+	}
+
+	{
+		var buf bytes.Buffer
+		logger := NewWithWriter(&buf)
+		logger.Error("error message")
+		logger.Info("info message")
+		logger.Debug("debug message")
+		fmt.Println(buf.String())
+	}
+
+	{
+		logger := NewCloudLoggingLogger()
+		logger.Error("error message")
+		logger.Info("info message")
+		logger.Debug("debug message")
+	}
+}
+
+func NewWithWriter(w io.Writer) *zap.Logger {
+	sink := zapcore.AddSync(w)
+
+	enc := zapcore.NewJSONEncoder(zap.NewProductionEncoderConfig())
+
+	core := zapcore.NewCore(enc, sink, zapcore.InfoLevel)
+
+	logger := zap.New(core)
+	return logger
+}
+
+func NewMultiOutputLogger() *zap.Logger {
+	highPriority := zap.LevelEnablerFunc(func(lvl zapcore.Level) bool {
+		return lvl >= zapcore.ErrorLevel
+	})
+	lowPriority := zap.LevelEnablerFunc(func(lvl zapcore.Level) bool {
+		return lvl < zapcore.ErrorLevel
+	})
+	stdoutSink := zapcore.Lock(os.Stdout)
+	stderrSink := zapcore.Lock(os.Stderr)
+
+	enc := zapcore.NewConsoleEncoder(zap.NewDevelopmentEncoderConfig())
+
+	core := zapcore.NewTee(
+		zapcore.NewCore(enc, stderrSink, highPriority),
+		zapcore.NewCore(enc, stdoutSink, lowPriority),
+	)
+
+	logger := zap.New(core)
+	return logger
+}
+
+func NewCloudLoggingLogger() *zap.Logger {
+	cfg := zap.NewProductionConfig()
+	cfg.EncoderConfig = newEncoderConfig()
+	logger, _ := cfg.Build()
+	return logger
+}
+
+var logLevelSeverity = map[zapcore.Level]string{
+	zapcore.DebugLevel:  "DEBUG",
+	zapcore.InfoLevel:   "INFO",
+	zapcore.WarnLevel:   "WARNING",
+	zapcore.ErrorLevel:  "ERROR",
+	zapcore.DPanicLevel: "CRITICAL",
+	zapcore.PanicLevel:  "ALERT",
+	zapcore.FatalLevel:  "EMERGENCY",
+}
+
+func EncodeLevel(l zapcore.Level, enc zapcore.PrimitiveArrayEncoder) {
+	enc.AppendString(logLevelSeverity[l])
+}
+
+func newEncoderConfig() zapcore.EncoderConfig {
+	cfg := zap.NewProductionEncoderConfig()
+	cfg.TimeKey = "timestamp"
+	cfg.LevelKey = "severity"
+	cfg.MessageKey = "message"
+	cfg.EncodeLevel = EncodeLevel
+	cfg.EncodeTime = zapcore.RFC3339NanoTimeEncoder
+
+	return cfg
+}
