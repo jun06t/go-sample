adad2a3585fb1d466e077c4e7f5ef57ebc729b49
Junpei Tsuji <junpei.tsuji.ams@gmail.com>
2023-03-14 13:09:32 +0900

    feat: add tracer

--- opentelemetry/gateway/go.mod
+++ opentelemetry/gateway/go.mod
@@ -4,13 +4,23 @@ go 1.20
 
 require (
 	github.com/jun06t/go-sample v0.0.0-20230313222239-4f72697f7c78
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.40.0
+	go.opentelemetry.io/otel v1.14.0
+	go.opentelemetry.io/otel/sdk v1.14.0
 	google.golang.org/grpc v1.53.0
 )
 
 require (
+	github.com/felixge/httpsnoop v1.0.3 // indirect
+	github.com/go-logr/logr v1.2.3 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	go.opentelemetry.io/otel/exporters/jaeger v1.14.0 // indirect
+	go.opentelemetry.io/otel/exporters/stdout/stdouttrace v1.14.0 // indirect
+	go.opentelemetry.io/otel/metric v0.37.0 // indirect
+	go.opentelemetry.io/otel/trace v1.14.0 // indirect
 	golang.org/x/net v0.5.0 // indirect
-	golang.org/x/sys v0.4.0 // indirect
+	golang.org/x/sys v0.5.0 // indirect
 	golang.org/x/text v0.6.0 // indirect
 	google.golang.org/genproto v0.0.0-20230110181048-76db0878b65f // indirect
 	google.golang.org/protobuf v1.28.1 // indirect
--- opentelemetry/gateway/main.go
+++ opentelemetry/gateway/main.go
@@ -6,7 +6,17 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
+	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/exporters/jaeger"
+	"go.opentelemetry.io/otel/exporters/stdout/stdouttrace"
+	"go.opentelemetry.io/otel/sdk/resource"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.4.0"
+	"go.opentelemetry.io/otel/trace"
 	"google.golang.org/grpc"
 
 	pb "github.com/jun06t/go-sample/opentelemetry/proto"
@@ -14,11 +24,18 @@ import (
 
 func main() {
 	backend := os.Getenv("BACKEND_ADDR")
+
+	_, cleanup, err := NewTracerProvider("otel-sample")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer cleanup()
+
 	h := newHandler(backend)
 
 	mux := http.NewServeMux()
 	mux.Handle("/", http.HandlerFunc(h.alive))
-	mux.Handle("/hello", http.HandlerFunc(h.hello))
+	mux.Handle("/hello", otelhttp.NewHandler(http.HandlerFunc(h.hello), "hello", otelhttp.WithMessageEvents(otelhttp.ReadEvents, otelhttp.WriteEvents)))
 	http.ListenAndServe(":8000", mux)
 }
 
@@ -35,7 +52,7 @@ func newHandler(addr string) *handler {
 	c := pb.NewGreeterClient(conn)
 
 	hc := http.Client{
-		Transport: http.DefaultTransport,
+		Transport: otelhttp.NewTransport(http.DefaultTransport),
 	}
 
 	return &handler{
@@ -54,14 +71,86 @@ func (h *handler) hello(w http.ResponseWriter, r *http.Request) {
 		Age:  10,
 		Man:  true,
 	}
-	_, err := h.cli.SayHello(context.Background(), req)
+	_, err := h.cli.SayHello(r.Context(), req)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	hreq, err := http.NewRequest("GET", "http://httpbin.org/delay/2", nil)
-	_, err = h.hcli.Do(hreq)
+	Sleep(r.Context())
+
+	hreq, err := http.NewRequestWithContext(r.Context(), "GET", "http://httpbin.org/delay/2", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
+	resp, err := h.hcli.Do(hreq)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+}
+
+var tracer trace.Tracer
+
+func Sleep(ctx context.Context) {
+	_, span := tracer.Start(ctx, "sleep")
+	defer span.End()
+	time.Sleep(1 * time.Second)
+}
+
+func NewTracerProvider(serviceName string) (*sdktrace.TracerProvider, func(), error) {
+	exporter, err := NewJaegerExporter()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	r := NewResource(serviceName, "v1", "local")
+	tp := sdktrace.NewTracerProvider(
+		sdktrace.WithBatcher(exporter),
+		sdktrace.WithResource(r),
+		sdktrace.WithSampler(sdktrace.TraceIDRatioBased(1)),
+	)
+
+	otel.SetTracerProvider(tp)
+	tracer = otel.Tracer("example.com/example-service")
+
+	cleanup := func() {
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		if err := tp.ForceFlush(ctx); err != nil {
+			log.Print(err)
+		}
+		ctx2, cancel2 := context.WithTimeout(context.Background(), 5*time.Second)
+		if err := tp.Shutdown(ctx2); err != nil {
+			log.Print(err)
+		}
+		cancel()
+		cancel2()
+	}
+	return tp, cleanup, nil
+}
+
+func NewResource(serviceName string, version string, environment string) *resource.Resource {
+	return resource.NewWithAttributes(
+		semconv.SchemaURL,
+		semconv.ServiceNameKey.String(serviceName),
+		semconv.ServiceVersionKey.String(version),
+		attribute.String("environment", environment),
+	)
+}
+
+func NewJaegerExporter() (sdktrace.SpanExporter, error) {
+	// Port details: https://www.jaegertracing.io/docs/getting-started/
+	endpoint := os.Getenv("EXPORTER_ENDPOINT")
+
+	exporter, err := jaeger.New(jaeger.WithCollectorEndpoint(jaeger.WithEndpoint(endpoint)))
+	if err != nil {
+		return nil, err
+	}
+	return exporter, nil
+}
+
+func NewStdoutExporter() (sdktrace.SpanExporter, error) {
+	return stdouttrace.New(
+		stdouttrace.WithPrettyPrint(),
+		stdouttrace.WithWriter(os.Stderr),
+	)
 }
