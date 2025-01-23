7a2f434af64588b44d5f9322d7ea58218ed04582
Junpei Tsuji <junpei.tsuji.ams@gmail.com>
2025-01-23 10:17:36 +0900

    feat: added openfga sample

---
+++ openfga/go.mod
@@ -0,0 +1,19 @@
+module github.com/jun06t/go-sample/openfga
+
+go 1.23.5
+
+require (
+	github.com/go-chi/chi/v5 v5.2.0
+	github.com/golang-jwt/jwt/v4 v4.5.1
+	github.com/openfga/go-sdk v0.6.3
+)
+
+require (
+	github.com/go-logr/logr v1.4.2 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
+	go.opentelemetry.io/otel v1.33.0 // indirect
+	go.opentelemetry.io/otel/metric v1.33.0 // indirect
+	go.opentelemetry.io/otel/trace v1.33.0 // indirect
+	golang.org/x/sync v0.10.0 // indirect
+)
---
+++ openfga/main.go
@@ -0,0 +1,164 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/golang-jwt/jwt/v4"
+	. "github.com/openfga/go-sdk/client"
+)
+
+const (
+	endpoint = "http://localhost:8080/v1/data/app/rbac/allow"
+)
+
+func main() {
+	r := chi.NewRouter()
+
+	// 本来リクエストから取得すべきJWTオブジェクトを、簡単のため埋め込むミドルウェアを追加
+	r.Use(embedJWT)
+
+	r.Route("/articles", func(r chi.Router) {
+		r.Use(preauthorize("article"))
+		r.Use(checkAuthorization)
+
+		r.Get("/", list)
+		r.Post("/", create)
+
+		r.Route("/{id}", func(r chi.Router) {
+			r.Get("/", get)
+			r.Put("/", update)
+			r.Delete("/", del)
+		})
+	})
+
+	r.Route("/items", func(r chi.Router) {
+		r.Use(preauthorize("item"))
+		r.Use(checkAuthorization)
+
+		r.Get("/", list)
+		r.Post("/", create)
+
+		r.Route("/{id}", func(r chi.Router) {
+			r.Get("/", get)
+			r.Put("/", update)
+			r.Delete("/", del)
+		})
+	})
+
+	err := http.ListenAndServe(":9000", r)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func get(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprint(w, fmt.Sprintf("got %s\n", id))
+}
+
+func list(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprint(w, "listed\n")
+}
+
+func create(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprint(w, "created\n")
+}
+
+func update(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprint(w, fmt.Sprintf("updated %s\n", id))
+}
+
+func del(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprint(w, fmt.Sprintf("deleted %s\n", id))
+}
+
+func embedJWT(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		claims := jwt.MapClaims{
+			"name": "alice",
+		}
+		token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+		ctx := context.WithValue(r.Context(), "user", token)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
+
+func preauthorize(resource string) func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			user := r.Context().Value("user").(*jwt.Token)
+			claims := user.Claims.(jwt.MapClaims)
+			name := claims["name"].(string)
+			ctx := context.WithValue(r.Context(), "username", name)
+
+			var relation string
+			switch r.Method {
+			case "GET":
+				relation = "viewer"
+			case "POST":
+				relation = "editor"
+			case "DELETE":
+				relation = "owner"
+			default:
+				relation = "owner"
+			}
+
+			ctx = context.WithValue(ctx, "relation", relation)
+			object := "api:" + resource
+			ctx = context.WithValue(ctx, "object", object)
+
+			next.ServeHTTP(w, r.WithContext(ctx))
+		})
+	}
+}
+
+func checkAuthorization(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fgaClient, err := NewSdkClient(&ClientConfiguration{
+			ApiUrl:  "http://127.0.0.1:8080",
+			StoreId: "01JJ7TD9J1T5H03V6V4H0PH37P",
+		})
+		if err != nil {
+			log.Printf("%#v\n", err)
+			http.Error(w, "Unable to build OpenFGA client", http.StatusServiceUnavailable)
+			return
+		}
+
+		username := r.Context().Value("username").(string)
+		relation := r.Context().Value("relation").(string)
+		object := r.Context().Value("object").(string)
+
+		body := ClientCheckRequest{
+			User:     "user:" + username,
+			Relation: relation,
+			Object:   object,
+		}
+
+		data, err := fgaClient.Check(context.Background()).Body(body).Execute()
+		if err != nil {
+			log.Printf("%#v\n", err)
+			http.Error(w, "Unable to check for authorization", http.StatusServiceUnavailable)
+			return
+		}
+
+		if !(*data.Allowed) {
+			log.Printf("%#v\n", data)
+			http.Error(w, "Forbidden to access document", http.StatusForbidden)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
