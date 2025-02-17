651e14a6ffa4769e60d22ff5c22dbb9c63d2ccc5
Junpei Tsuji <junpei.tsuji.ams@gmail.com>
2025-02-17 09:53:02 +0900

    chore: added graceful shutdown

--- openfeature/basic/main.go
+++ openfeature/basic/main.go
@@ -4,16 +4,25 @@ import (
 	"context"
 	"log"
 	"net/http"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	flagd "github.com/open-feature/go-sdk-contrib/providers/flagd/pkg"
 	"github.com/open-feature/go-sdk/openfeature"
 )
 
-const defaultMessage = "Hello!"
-const newWelcomeMessage = "Hello, welcome to this OpenFeature-enabled website!"
+const (
+	defaultMessage    = "Hello!"
+	newWelcomeMessage = "Hello, welcome to this OpenFeature-enabled website!"
+)
 
 func main() {
+	// シグナルを受け取るcontextを作成
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
 	err := openfeature.SetNamedProviderAndWait("my-domain", flagd.NewProvider())
 	if err != nil {
 		log.Fatalf("Failed to set the OpenFeature provider: %v", err)
@@ -38,5 +47,28 @@ func main() {
 		}
 	})
 
-	engine.Run()
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: engine,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Failed to start server: %v", err)
+		}
+	}()
+
+	<-ctx.Done()
+	log.Println("Shutting down server...")
+
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(shutdownCtx); err != nil {
+		log.Fatal("Server forced to shutdown:", err)
+	}
+	// graceful shutdown of active providers.
+	openfeature.Shutdown()
+
+	log.Println("Server exiting")
 }
